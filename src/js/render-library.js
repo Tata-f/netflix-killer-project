3074996a6b4255fa2f05f48daed5f3c2c6526e2c
unknown <74738757+leonid-br@users.noreply.github.com>
2021-07-14 23:05:32 +0300

    доделал перерендер в библе

--- src/js/render-library.js
+++ src/js/render-library.js
@@ -9,7 +9,7 @@ renderLibraryEl.addEventListener('click', onClickLibrary);
 renderWatchedEl.addEventListener('click', onClickWatched);
 renderQueueEl.addEventListener('click', onClickQueue);
 
-async function onClickLibrary() {
+ function onClickLibrary() {
   renderWatchedEl.classList.add('active');
   renderQueueEl.classList.remove('active');
 
@@ -59,6 +59,6 @@ function createCardMarkup(results) {
   renderContainer.innerHTML = elements;
   const voteAverageRef = document.querySelectorAll('.vote-average');
   voteAverageRef.forEach(el => el.classList.add('is-visible'));
-  console.log(elements);
-  // console.log(voteAverage  Ref);
 }
+
+export {onClickWatched, onClickQueue}
