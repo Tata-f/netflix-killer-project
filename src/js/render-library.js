76dd7829db425c22fd8e6ed470b6ffe2b6ed3afd
unknown <74738757+leonid-br@users.noreply.github.com>
2021-07-14 19:00:02 +0300

    доделан рендер без обновления

--- src/js/render-library.js
+++ src/js/render-library.js
@@ -3,12 +3,10 @@ const renderWatchedEl = document.querySelector('.render-watched-js');
 const renderQueueEl = document.querySelector('.render-queue-js');
 const renderContainer = document.querySelector('.gallery-section > .container');
 import movieCardTpl from '../templates/movie-card.hbs';
-import FilmApiService from './class-api-service';
 
 renderLibraryEl.addEventListener('click', onClickLibrary);
-// renderWatchedEl.addEventListener('click', onClickWatched);
-// renderQueueEl.addEventListener('click', onClickQueue);
-const filmApiService = new FilmApiService();
+renderWatchedEl.addEventListener('click', onClickWatched);
+renderQueueEl.addEventListener('click', onClickQueue);
 
 async function  onClickLibrary() {
   renderWatchedEl.classList.add('active');
@@ -16,46 +14,44 @@ async function  onClickLibrary() {
 
   const filmsStr = localStorage.getItem('watchedFilm');
   const filmsArr = JSON.parse(filmsStr);
-  const genresList = await filmApiService.fetchGenres()
-  const { genres } = genresList;
+  const moviesWithYearAndGenre = getUpdatedLibraryMovieInfo(filmsArr);
   
-  const moviesWithYearAndGenre = getUpdatedMovieInfo(filmsArr, genres);
-
-
   createCardMarkup(moviesWithYearAndGenre);
 }
 
-// function onClickWatched() {
-//   renderWatchedEl.classList.add('active');
-//   renderQueueEl.classList.remove('active');
-//   renderContainer.innerHTML = '';
-//   const filmsStr = localStorage.getItem('watchedFilm');
-//   const filmsArr = JSON.parse(filmsStr);
-//   // renderDefaultMovies(filmsStr);
-//   renderContainer.insertAdjacentHTML('beforeend', movieCardTpl(filmsArr));
-// }
+function onClickWatched() {
+  renderWatchedEl.classList.add('active');
+  renderQueueEl.classList.remove('active');
+  renderContainer.innerHTML = '';
 
-// function onClickQueue() {
-//   renderWatchedEl.classList.remove('active');
-//   renderQueueEl.classList.add('active');
-//   renderContainer.innerHTML = '';
-//   const filmsStr = localStorage.getItem('queueFilm');
-//   const filmsArr = JSON.parse(filmsStr);
-//   // renderDefaultMovies(filmsStr);
+  const filmsStr = localStorage.getItem('watchedFilm');
+  const filmsArr = JSON.parse(filmsStr);
+  const moviesWithYearAndGenre = getUpdatedLibraryMovieInfo(filmsArr);
+  
+  createCardMarkup(moviesWithYearAndGenre);
 
-//   renderContainer.insertAdjacentHTML('beforeend', movieCardTpl(filmsArr));
-// }
+}
 
-function getUpdatedMovieInfo(movies, info) {
+function onClickQueue() {
+  renderWatchedEl.classList.remove('active');
+  renderQueueEl.classList.add('active');
+  renderContainer.innerHTML = '';
+
+  const filmsStr = localStorage.getItem('queueFilm');
+  const filmsArr = JSON.parse(filmsStr);
+  const moviesWithYearAndGenre = getUpdatedLibraryMovieInfo(filmsArr);
+  
+  createCardMarkup(moviesWithYearAndGenre);
+}
+
+function getUpdatedLibraryMovieInfo(movies) {
   return movies.map(movie => ({
     ...movie,
-    genres: info
-      .filter(genre => movie.genre_ids.includes(genre.id))
-      .map(({ name }) => name)
-      .join(', '),
+    genres: movie.genres.map(({ name }) => name).join(', '),
     releaseYear: movie.release_date ? movie.release_date.slice(0, 4) : 'n/a',
   }));
 }
+
 
 function createCardMarkup(results) {
   renderContainer.innerHTML='';
