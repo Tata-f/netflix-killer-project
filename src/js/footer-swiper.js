bd47f0d57a1accdace97afed7ebad94ffd88802b
Gulnaz-Sydykbek <gulnaz.sydykbekova.94@mail.ru>
2021-07-18 19:41:53 +0600

    swipe

--- src/js/footer-swiper.js
+++ src/js/footer-swiper.js
@@ -14,16 +14,6 @@ function onClickOpen() {
   document.body.classList.add('scroll-hidden');
 
   const swiper = new Swiper('.image-slider', {
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    pagination: {
-      el: '.swiper-pogination',
-      clickable: true,
-    },
-
     keyboard: {
       enabled: true,
       onlyInViewport: true,
@@ -47,13 +37,6 @@ function onClickOpen() {
       shadowOffset: 29,
       shadowScale: 0.94,
     },
-
-    autoplay: {
-      delay: 1000,
-      disableOnInteraction: true,
-    },
-
-    speed: 800,
 
     loop: true,
  });
